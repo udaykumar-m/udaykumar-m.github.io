43c60350271addf9a4a66f1fbc68e30ec058f339
udaykumar-m <udaykumardevil3830@gmail.com>
2024-04-17 13:54:56 +0530

    trying out the particle background

---
+++ particle_background/js/demo.js
@@ -0,0 +1,12 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+// jQuery plugin example:
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+})
